12e27fa52ff390eb19618ba7482608cae34489bb
daniel.kansaon@mq.local <daniel.kansaon@mq.local>
2018-06-15 21:09:33 -0300

    Swagger

--- Startup.cs
+++ Startup.cs
@@ -68,7 +68,7 @@ namespace Api_Livraria
 
            app.UseSwaggerUI(c =>
            {
-               c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+               c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api-Livraria");
                c.RoutePrefix = string.Empty;
            });
 
